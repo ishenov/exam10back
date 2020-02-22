adfd305eae7d6074c33a86d345d20458558b6810
Baktiyar Ishenov <b1963826@gmail.com>
2020-02-22 13:58:16 +0600

    back of news

--- app/news.js
+++ app/news.js
@@ -19,12 +19,12 @@ const router = express.Router();
 const upload = multer({storage});
 
 router.get('/news', async (req, res) => {
-	const news = await mysql.getConnection().query('SELECT `name`, `id`, `category_id`, `location_id` FROM `items`');
+	const news = await mysql.getConnection().query('SELECT `title`, `news_id`, `image`, `datetime` FROM `news`');
 	res.send(news);
 });
 
 router.get('/news/:id', async (req, res) => {
-	const news = await mysql.getConnection().query('SELECT * FROM `items` WHERE `id` = ?', req.params.id);
+	const news = await mysql.getConnection().query('SELECT * FROM `news` WHERE `id` = ?', req.params.id);
 	if (!news[0]){
 		res.send({message : "not found"});
 	}
@@ -36,12 +36,12 @@ router.post('/news', upload.single('image'), async (req, res) => {
 	if (req.file) {
 		message.image = req.file.filename;
 	}
-	if (message.name && message.categoryId && message.locationId){
+	if (message.title && message.newsId && message.content){
 		const date = Date.now().toString();
 		const result = await mysql.getConnection().query(
-			'INSERT INTO `items` (`name`,`datetime`,`description`,`category_id`, `location_id`,`image`) VALUES ' +
-			'(?,?,?,?,?,?)',
-			[message.name,date,message.description,message.categoryId,message.locationId,message.image]);
+			'INSERT INTO `news` (`title`, `news_id`, `image`, `datetime`, `content`) VALUES ' +
+			'(?,?,?,?,?)',
+			[message.title,message.newsId,message.image,date,message.content]);
 		res.send({id : result.insertId,...message});
 	}
 	res.send({message : "fill all required fields"});
@@ -49,7 +49,7 @@ router.post('/news', upload.single('image'), async (req, res) => {
 router.delete('/news/:id', async (req,res) => {
 	try{
 		await mysql.getConnection().query(
-			'DELETE FROM `items` WHERE `id` = ?',req.params.id
+			'DELETE FROM `news` WHERE `id` = ?',req.params.id
 		);
 		res.send({message : 'OK'});
 	} catch (e) {
